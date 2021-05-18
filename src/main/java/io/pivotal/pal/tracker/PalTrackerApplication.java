cbb010bdcabe0d159fbfc49ceed9f4a2086be9e6
sidney.hall <borelhall@outlook.com>
2021-05-18 14:50:08 +0000

    Final Commit for Configuring App Lab Including Unversioned Files

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,11 +1,15 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PalTrackerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
 }
